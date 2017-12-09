fc688926bc98087483e58ec990994eb2ba5b1a71
DESKTOP-IF2UK7B\sungsoyeon <soyun0571@naver.com>
2017-12-09 22:19:06 +0900

    10:18

---
+++ ex01/models/like-log.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var schema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  event: { type: Schema.Types.ObjectId, ref: 'event' },
+  createdAt: {type: Date, default: Date.now}
+}, {
+  toJSON: { virtuals: true},
+  toObject: {virtuals: true}
+});
+var LikeLog = mongoose.model('LikeLog', schema);
+
+module.exports = LikeLog;
